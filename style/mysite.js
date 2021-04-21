49128faa0c01db3f375e4379398078e47260df74
ambahjones <67397295+ambahjones@users.noreply.github.com>
2021-04-21 15:30:08 -0400

    working toward making site interactive, added js file basics and modified html and css to support ideas for js functionality

---
+++ style/mysite.js
@@ -0,0 +1,7 @@
+let weather = document.querySelector('#weather');
+let tea = document.querySelector('#tea-cozy');
+let company = document.querySelector('#company-page');
+let club = document.querySelector('#club-page');
+let portfolio = document.querySelector('#portfolio');
+
+let projects = [weather, tea, company, club, portfolio];
