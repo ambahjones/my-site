7cec85755a462f928e1c442f10fdce6b2e88adf5
ambahjones <67397295+ambahjones@users.noreply.github.com>
2021-07-06 15:03:23 -0400

    cleared js file to prepare for new approach to interactivity

--- style/mysite.js
+++ style/mysite.js
@@ -1,7 +1,1 @@
-let weather = document.querySelector('#weather');
-let tea = document.querySelector('#tea-cozy');
-let company = document.querySelector('#company-page');
-let club = document.querySelector('#club-page');
-let portfolio = document.querySelector('#portfolio');
 
-let projects = [weather, tea, company, club, portfolio];
